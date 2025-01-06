3417db2f59aa564cfffff2ae5f7e2c00633bad25
Suganya2203 <suganyathabasumuthu22@gmail.com>
2025-01-06 22:02:10 +0530

    Add files via upload
    Week 1 assignments

---
+++ week1HomeAssignment/Chrome.java
@@ -0,0 +1,22 @@
+package week1HomeAssignment;
+
+public class Chrome {
+
+	public static void main(String[] args) 
+	{
+
+		float version=91.0f;
+		String developer="Chrome";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKeys='C';
+		
+		System.out.println(version+ " is the version of Google" );
+		System.out.println(developer+ " is the developer");
+		System.out.println( "is beta? "+isBeta);
+		System.out.println(releaseYear+ " is the release year");
+		System.out.println(shortcutKeys+ " is the short cut keys");
+
+	}
+
+}
---
+++ week1HomeAssignment/IsPositive.java
@@ -0,0 +1,22 @@
+package week1HomeAssignment;
+
+public class IsPositive {
+
+	public static void main(String[] args) 
+	
+	{
+		int a=10;
+		
+		if(a>=0)
+			
+		{
+			System.out.println(a+ " is positive number");
+		}
+		else
+		{
+			System.out.println(a+ " is a negative number");
+		}
+
+	}
+
+}
