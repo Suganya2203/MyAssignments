c61501287590418635e16c9f78e8134cae65b024
Suganya2203 <suganyathabasumuthu22@gmail.com>
2025-01-09 23:12:33 +0530

    Add files via upload

---
+++ week1HomeAssignment/IsPrime.java
@@ -0,0 +1,28 @@
+package week1HomeAssignment;
+
+import java.util.Iterator;
+
+public class IsPrime 
+{
+
+	public static void main(String[] args) 
+	{
+		int n = 13;
+		for (int i=13;i<=13; i++) 
+		
+			{
+		
+			if (i% n==0) 
+			{
+				System.out.println(i + " is a prime");
+				
+			}
+			
+			else 
+			{
+				System.out.println(i+ " is not a prime number");
+			}
+	        }
+	}
+}
+	
